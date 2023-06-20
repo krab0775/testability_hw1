6de0a173c6489eaaeca97ef1f1b5bd8b21fa7273
krab0775 <krab0775@gmail.com>
2023-06-20 15:49:20 +0700

    add BonusMilesService

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int bonus;
+        if (price >= 20) {
+            bonus = (price / 20);
+        } else {
+            bonus = 0;
+            }
+        return bonus;
+    }
+
+    }
