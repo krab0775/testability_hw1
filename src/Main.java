348a179be16ddc2ca87335531231250039d18f57
krab0775 <krab0775@gmail.com>
2023-06-20 15:49:32 +0700

    edit main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticket = 74764; // Цена билета
-        int mile = 20; // Кол-во рублей за одну милю
-
-        int bonus;
-        if (ticket >= mile) {
-            bonus = (ticket / mile);
-            System.out.println(bonus +" бонусов начислено");
-        } else {
-            System.out.println("Бонусов не начислено");
-        }
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 755;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
